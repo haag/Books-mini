0f9e1808ea2706a6f789e29f5b6705724a53352f
K <codehaag@gmail.com>
2018-11-10 21:52:21 -0700

    Moved book-list to containers, added react-redux, mapping state to props

--- tinder/src/components/App.js
+++ tinder/src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import '../App.css';
-import BookList from "./book-list"
+import BookList from "../containers/book-list"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <BookList />
-
+        <BookList />
       </div>
     );
   }
